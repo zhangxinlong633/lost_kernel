59cfd5cc5068e06317e39efdc9a7c53a715c5164
zhangxinlong633 <15205157627@163.com>
2022-02-04 22:04:30 +0800

    hal

--- hal.h
+++ hal.h
@@ -5,6 +5,8 @@ struct lost_kernel_hal_ops {
   int switch_task(void *task_before, void *task_after);
   int timer(void *data);
   void *phys_memory_base(void);
+  int cpu_num();
+  int bind_task_to_cpu(void *task, int cpu);
 };
 
 int lost_kernel_hal_register(struct lost_kernel_hal_ops *ops);
