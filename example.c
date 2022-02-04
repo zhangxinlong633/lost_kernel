12d014ba6827d5e318ea5ec372168cec5918653d
zhangxinlong633 <15205157627@163.com>
2022-02-04 22:13:36 +0800

    example.c

---
+++ example.c
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+    void *buf = malloc(1024);
+    if (buf == NULL) {
+        goto exit;  
+    }
+    
+    printf("malloc memory success\n");
+    
+exit:
+    return 0;
+}
